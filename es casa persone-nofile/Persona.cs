965533a06a42c523820126e0981ce901bfffa96b
Acer <Acer@LAPTOP-9SISV8A7>
2024-02-01 18:05:46 +0100

    Aggiungere i file di progetto.

---
+++ es casa persone-nofile/Persona.cs
@@ -0,0 +1,32 @@
+﻿using Systems;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_casa_persone_nofile
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public int Eta { get; set;}
+
+        public Persona(string nome, int eta)
+        {
+            Nome = nome;
+            Eta = eta;
+        }
+
+        public static Persona Parse(string str)
+        {
+            string[] caratteristiche = str.Split(',');
+            string nome = caratteristiche[0];
+            int eta = int.Parse(caratteristiche[1]);
+            return new Persona(nome, eta);
+        }
+        public override string ToString()
+        {
+            return string.Format("{0},{1}", Nome, Eta); 
+        }
+    }
+}
---
+++ es casa persone-nofile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_casa_persone_nofile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Persona> Persone = new List<Persona>();
+            Console.WriteLine("n' persone:");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("caratteristiche:");
+                Persona p = Persona.Parse(Console.ReadLine());
+                Persone.Add(p);
+            }
+            List<Persona> persone_maggiorenni = new List<Persona>();
+            foreach (var Persona in Persone)
+            {
+                if (Persona.Eta >= 18)
+                    Console.WriteLine(Persona.Nome);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
